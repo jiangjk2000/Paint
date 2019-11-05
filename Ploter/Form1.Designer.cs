4c3e4f954871e68c3280d19d1a437c86ec1fcafc
jiangjk2000 <jiangjk2000@outlook.com>
2019-11-05 22:14:51 +0800

    Update First Edition
    1.0

---
+++ Ploter/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Ploter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.PlotItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CircleItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RectItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LinePlotItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SetItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PenSetItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LineWidthItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PenWidthItem = new System.Windows.Forms.ToolStripComboBox();
+            this.AboutItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.PenColorStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.MousePositionItem = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Plotlabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PlotItem,
+            this.SetItem,
+            this.AboutItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // PlotItem
+            // 
+            this.PlotItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CircleItem,
+            this.RectItem,
+            this.LinePlotItem});
+            this.PlotItem.Name = "PlotItem";
+            this.PlotItem.Size = new System.Drawing.Size(71, 24);
+            this.PlotItem.Text = "Plot(&P)";
+            // 
+            // CircleItem
+            // 
+            this.CircleItem.Name = "CircleItem";
+            this.CircleItem.Size = new System.Drawing.Size(224, 26);
+            this.CircleItem.Text = "Circle";
+            this.CircleItem.Click += new System.EventHandler(this.CircleItem_Click);
+            // 
+            // RectItem
+            // 
+            this.RectItem.Name = "RectItem";
+            this.RectItem.Size = new System.Drawing.Size(224, 26);
+            this.RectItem.Text = "Rectangle";
+            this.RectItem.Click += new System.EventHandler(this.RectItem_Click);
+            // 
+            // LinePlotItem
+            // 
+            this.LinePlotItem.Name = "LinePlotItem";
+            this.LinePlotItem.Size = new System.Drawing.Size(224, 26);
+            this.LinePlotItem.Text = "Line";
+            this.LinePlotItem.Click += new System.EventHandler(this.lineToolStripMenuItem_Click_1);
+            // 
+            // SetItem
+            // 
+            this.SetItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PenSetItem});
+            this.SetItem.Name = "SetItem";
+            this.SetItem.Size = new System.Drawing.Size(95, 24);
+            this.SetItem.Text = "Setting(&S)";
+            // 
+            // PenSetItem
+            // 
+            this.PenSetItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.LineWidthItem});
+            this.PenSetItem.Name = "PenSetItem";
+            this.PenSetItem.Size = new System.Drawing.Size(224, 26);
+            this.PenSetItem.Text = "Pen";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.colorToolStripMenuItem.Text = "Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+            // 
+            // LineWidthItem
+            // 
+            this.LineWidthItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PenWidthItem});
+            this.LineWidthItem.Name = "LineWidthItem";
+            this.LineWidthItem.Size = new System.Drawing.Size(224, 26);
+            this.LineWidthItem.Text = "LineWidth";
+            this.LineWidthItem.Click += new System.EventHandler(this.lineToolStripMenuItem_Click);
+            // 
+            // PenWidthItem
+            // 
+            this.PenWidthItem.Name = "PenWidthItem";
+            this.PenWidthItem.Size = new System.Drawing.Size(121, 28);
+            // 
+            // AboutItem
+            // 
+            this.AboutItem.Name = "AboutItem";
+            this.AboutItem.Size = new System.Drawing.Size(90, 24);
+            this.AboutItem.Text = "About(&A)";
+            this.AboutItem.Click += new System.EventHandler(this.AboutItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(26, 422);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PenColorStatus,
+            this.MousePositionItem});
+            this.statusStrip1.Location = new System.Drawing.Point(26, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(774, 26);
+            this.statusStrip1.TabIndex = 2;
+            // 
+            // PenColorStatus
+            // 
+            this.PenColorStatus.LinkColor = System.Drawing.Color.Red;
+            this.PenColorStatus.Name = "PenColorStatus";
+            this.PenColorStatus.Size = new System.Drawing.Size(145, 20);
+            this.PenColorStatus.Text = "Pen Color:255 0 0  ";
+            // 
+            // MousePositionItem
+            // 
+            this.MousePositionItem.Name = "MousePositionItem";
+            this.MousePositionItem.Size = new System.Drawing.Size(148, 20);
+            this.MousePositionItem.Text = "MousePosition:0, 0";
+            // 
+            // Plotlabel
+            // 
+            this.Plotlabel.BackColor = System.Drawing.Color.White;
+            this.Plotlabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Plotlabel.Location = new System.Drawing.Point(26, 28);
+            this.Plotlabel.Name = "Plotlabel";
+            this.Plotlabel.Size = new System.Drawing.Size(774, 396);
+            this.Plotlabel.TabIndex = 3;
+            this.Plotlabel.Paint += new System.Windows.Forms.PaintEventHandler(this.Plotlabel_Paint);
+            this.Plotlabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.Plotlabel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Plotlabel_MouseMove);
+            this.Plotlabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Plotlabel);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Plot";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem AboutItem;
+        private System.Windows.Forms.ToolStripMenuItem PlotItem;
+        private System.Windows.Forms.ToolStripMenuItem CircleItem;
+        private System.Windows.Forms.ToolStripMenuItem RectItem;
+        private System.Windows.Forms.ToolStripMenuItem SetItem;
+        private System.Windows.Forms.ToolStripMenuItem PenSetItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem LineWidthItem;
+        private System.Windows.Forms.ToolStripComboBox PenWidthItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel PenColorStatus;
+        private System.Windows.Forms.Label Plotlabel;
+        private System.Windows.Forms.ToolStripStatusLabel MousePositionItem;
+        private System.Windows.Forms.ToolStripMenuItem LinePlotItem;
+    }
+}
+
---
+++ Ploter/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Ploter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region 事件函数
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pointOld.X = e.X;
+            pointOld.Y = e.Y;
+            circle.X = pointOld.X;
+            circle.Y = pointOld.Y;
+            rect.x = pointOld.X;
+            rect.y = pointOld.Y;
+            line.Old = pointOld;
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pointNew.X = e.X;
+            pointNew.Y = e.Y;
+            circle.width = pointNew.X - pointOld.X;
+            circle.height = pointNew.Y - pointOld.Y;
+            rect.height = pointNew.X - pointOld.X;
+            rect.width = pointNew.Y - pointOld.Y;
+            line.New = pointNew;
+            circle.pen = this.pen;
+            rect.pen = this.pen;
+            line.pen = this.pen;
+            if (plotCondition == 1)
+            {
+                StreamWriter sw = new StreamWriter("1.txt", true);
+                sw.WriteLine(circle);
+                sw.Close();
+            }
+            if (plotCondition == 2)
+            {
+                StreamWriter sw = new StreamWriter("1.txt", true);
+                sw.WriteLine(rect);
+                sw.Close();
+            }
+            if(plotCondition == 3)
+            {
+                StreamWriter sw = new StreamWriter("1.txt", true);
+                sw.WriteLine(line);
+                sw.Close();
+            }
+            Plotlabel.Invalidate();
+        }
+        private void Plotlabel_Paint(object sender, PaintEventArgs e)
+        {
+            circle.Draw(e, pen);
+            rect.Draw(e, pen);
+            line.Draw(e, pen);
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("1.txt", false);
+            sw.Close();
+        }
+        private void CircleItem_Click(object sender, EventArgs e)
+        {
+            plotCondition = 1;
+            //1为Circle
+            SingleCheck(sender);
+        }
+        private void RectItem_Click(object sender, EventArgs e)
+        {
+            plotCondition = 2;
+            SingleCheck(sender);
+        }
+        private void lineToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            plotCondition = 3;
+            SingleCheck(sender);
+        }
+        private void SingleCheck(object sender)
+        {
+            CircleItem.Checked = false;
+            RectItem.Checked = false;
+            LinePlotItem.Checked = false;
+            ((ToolStripMenuItem)sender).Checked = true;
+        }
+        private void AboutItem_Click(object sender, EventArgs e)
+        {
+            (new FormAbout()).ShowDialog();
+        }
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog1 = new ColorDialog();
+            colorDialog1.FullOpen = true;                       //是否显示ColorDialog有半部分
+            //colorDialog1.CustomColors = colorDialog1.Color;   //设置自定义颜色
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)                      //确定事件响应
+            {
+                Color color_from = colorDialog1.Color;
+                pen.Color = color_from;
+                PenColorStatus.Text = String.Format("Pen Color:{0} {1} {2}", pen.Color.R, pen.Color.G, pen.Color.B);
+            }
+        }
+        private void lineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void Plotlabel_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePositionItem.Text = String.Format("MousePositin:{0}, {1}", e.X, e.Y);
+        }
+        #endregion
+        #region 全局变量
+        Point pointOld = new Point();
+        Point pointNew = new Point();
+        Circle circle = new Circle();
+        Rect rect = new Rect();
+        Line line = new Line();
+        private int plotCondition = -1;
+        private Pen pen = new Pen(Color.Red);
+        #endregion
+        //Graphics g;
+        //private void Form1_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    g = this.CreateGraphics();
+        //    Pen p = new Pen(Color.Blue, 2);
+        //    if (plotCondition == 1)
+        //    {
+        //        g.DrawEllipse(Pens.Black, 20, 30, 40, 50);
+        //    }
+        //}
+    }
+    public class Graphical
+    {
+        public void PenDataUpdate(string[] a, Pen pen)
+        {
+            int R = int.Parse(a[5]);
+            int G = int.Parse(a[6]);
+            int B = int.Parse(a[7]);
+            float penWidth = float.Parse(a[8]);
+            pen.Width = penWidth;
+            pen.Color = Color.FromArgb(R, G, B);
+        }
+    }
+    interface IGraphicalPlot
+    {
+        void Draw(PaintEventArgs e,Pen pen);
+    }
+    public class Circle : Graphical, IGraphicalPlot
+    {
+        public int X;
+        public int Y;
+        public int height;
+        public int width;
+        public Pen pen;
+        public Circle()
+        { }
+        public Circle(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return String.Format("c,{0},{1},{2},{3},{4},{5},{6},{7}", X, Y, height, width,pen.Color.R, pen.Color.G, pen.Color.B, pen.Width);
+        }
+        public void Draw(PaintEventArgs e, Pen pen)
+        {
+            using (StreamReader sr = new StreamReader("1.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] a = line.Split(',');
+                    if (a[0] == "c")
+                    {
+                        float x = float.Parse(a[1]);
+                        float y = float.Parse(a[2]);
+                        float width = float.Parse(a[3]);
+                        float height = float.Parse(a[4]);
+                        this.PenDataUpdate(a, pen);
+                        e.Graphics.DrawEllipse(pen, x, y, height, width);
+                    }
+                }
+            }
+        }
+    }
+    public class Rect : Graphical, IGraphicalPlot
+    {
+        public int x;
+        public int y;
+        public int height;
+        public int width;
+        public Pen pen;
+        public override string ToString()
+        {
+            return string.Format("r,{0},{1},{2},{3},{4},{5},{6},{7}", x, y, height, width, pen.Color.R, pen.Color.G, pen.Color.B, pen.Width);
+        }
+        public void Draw(PaintEventArgs e,Pen pen)
+        {
+            using (StreamReader sr = new StreamReader("1.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] a = line.Split(',');
+                    if (a[0] == "r")
+                    {
+                        int x = int.Parse(a[1]);
+                        int y = int.Parse(a[2]);
+                        int height = int.Parse(a[3]);
+                        int width = int.Parse(a[4]);
+                        this.PenDataUpdate(a, pen);
+                        e.Graphics.DrawRectangle(pen, x, y, height, width);
+                    }
+                }
+            }
+        }
+    }
+    public class Line : Graphical, IGraphicalPlot
+    {
+        public Point Old;
+        public Point New;
+        public Pen pen;
+        public override string ToString()
+        {
+            return String.Format("l,{0},{1},{2},{3},{4},{5},{6},{7}", Old.X, Old.Y, New.X, New.Y, pen.Color.R, pen.Color.G, pen.Color.B, pen.Width);
+        }
+        public void Draw(PaintEventArgs e, Pen pen)
+        {
+            using (StreamReader sr = new StreamReader("1.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] a = line.Split(',');
+                    if (a[0] == "l")
+                    {
+                        int x1 = int.Parse(a[1]);
+                        int y1 = int.Parse(a[2]);
+                        int x2 = int.Parse(a[3]);
+                        int y2 = int.Parse(a[4]);
+                        this.PenDataUpdate(a, pen);
+                        e.Graphics.DrawLine(pen, x1, y1, x2, y2);
+                    }
+                }
+            }
+        }
+    }
+}
